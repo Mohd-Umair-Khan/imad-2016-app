3151d1e5dabd3f65687a84090b59bfeb1c418674
Mohd Umair <mohdumair1981999@gmail.com>
2016-12-01 17:11:15 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,5 +2,12 @@ console.log('Loaded!');
 
 //Move the image
 var img=document.getElementByid('madi');
+marginleft=0;
+function moveright(){
+    marginleft+=10;
+    img.style.marginleft=marginleft+'px';
+}
 img.onclick=function(){
-      img.style.marginLeft='100px'};
+      img.style.marginLeft='100px';
+    var interval =setinterval(moveright,100);
+};
